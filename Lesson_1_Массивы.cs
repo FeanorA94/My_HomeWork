12131060e163e3d28bfff9b4bb7436ca0d5409c3
FeanorA94 <152093019+FeanorA94@users.noreply.github.com>
2024-04-17 12:40:01 +0300

    Записная Книжка
    Добавили номера и контакты

--- Lesson_1_Массивы.cs
+++ Lesson_1_Массивы.cs
@@ -1,73 +1,11 @@
-// //Задача 2: Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
-
-//     namespace EvenNumbersInArray
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             int[] array = new int[10]{4, 16, 30, 12, 9, 22, 46, 58, 74, 82};
-//             int evenCount = 0;
-
-//             foreach (int number in array)
-//             {
-//                 if (number % 2 == 0)
-//                 {
-//                     evenCount++;
-//                 }
-//             }
-
-//             Console.WriteLine($"Количество четных чисел: {evenCount}");
-//         }
-//     }
-// }
-
-// //---------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// //Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
-
-//     using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         // Создаем массив с 10 вещественными числами
-//         Random rnd = new Random();
-//         double[] array = Enumerable.Range(0, 10).Select(x => rnd.NextDouble() * 10 - 5).ToArray();
-
-//         // Находим минимальное и максимальное значение в массиве
-//         double minValue = array.Min();
-//         double maxValue = array.Max();
-
-//         // Вычисляем разницу между ними
-//         double difference = maxValue - minValue;
-
-//         Console.WriteLine("Минимальный элемент: " + minValue);
-//         Console.WriteLine("Максимальный элемент: " + maxValue);
-//         Console.WriteLine("Разница между ними: " + difference);
-//     }
-// }
-
-// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// // Задача 1. Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, 
-// //значения которых лежат в отрезке [20,90].
-
-
-// int[] arr = new int[10];
-//             Random random = new Random();
-//             int count = 0;
-
-//             for (int i = 0; i < 10; i++)
-//             {
-//                 arr[i] = random.Next(1, 101);
-//                 if (arr[i] >= 20 && arr[i] <= 90)
-//                 {
-//                     count++;
-//                 }
-//             }
-
-//             Console.WriteLine(String.Join(", ", arr));
-//             Console.WriteLine(count);
-
+Belkin,Alexandr,+78888888888
+Frolov,Sergey,88888888888
+Garitchev,Leonid,8-900-44-444-77
+Khorobryih,Stas,89996666666
+Petrov,Aleksey,8-999-888-7777
+Romanova,Nataliya,+7-777-77-777-77
+Shirokov,Sergey,8-900-55-555-55
+Sidorov,Vitaliy,8-900-32-659-87
+Sidorova,Yuliya,+71231212312
+Tatarinov,Maksim,+71234512344
+Yakubovskiy,Sergey,8-888-77-666-55
